62fb294564af3e5db14f46ab2cd57bae787b8f40
Chris Wei <yunzhi.wei@live.com>
2017-06-26 16:24:50 +0800

    Sync up data with backend Firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      speed: 60
+    }
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      console.log("sync up")
+      this.setState({speed: snap.val()});
+    });
+  }
+
+  onSpeedInputKeyPress(e) {
+    if (e.which === 13) {
+      const rootRef = firebase.database().ref().child('react');
+      rootRef.set({ speed: e.target.value });
+      e.target.value = "";
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +39,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <h1>{this.state.speed}</h1>
+          <br />
+          <input onKeyPress= {(e) => this.onSpeedInputKeyPress(e)} />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyA79AjEPLb1YzCOLMC34mIOp94YZMC1CUY",
+  authDomain: "sync-with-mobx.firebaseapp.com",
+  databaseURL: "https://sync-with-mobx.firebaseio.com",
+  projectId: "sync-with-mobx",
+  storageBucket: "sync-with-mobx.appspot.com",
+  messagingSenderId: "920005046653"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
