ba59342ddc6ed7516cb919b9eb50e4ab05d851f4
bruce-zxy <bruce_zxy@163.com>
2017-07-17 16:51:32 +0800

    update success

--- src/pages/BigData/DynamicChartComponent.js
+++ src/pages/BigData/DynamicChartComponent.js
@@ -1,77 +1,49 @@
 import React,{ Component } from 'react';
 import ReactEcharts from './lib';
 
-function extend(target, source) {
-    for (var obj in source) {
-        target[obj] = source[obj];
-    }
-    return target;
+function extendObject(target, source) {
+    var newObj = new Object();
+    for (var obj in target) newObj[obj] = target[obj];
+    for (var obj in source) newObj[obj] = source[obj];
+    return newObj;
 }
 
 class DynamicChartComponent extends Component {
     
     constructor(props) {
         super(props)
-        this.state = {
-            option: {},
-            timeTicket: null,
-        }
+        this.state = { option: {}, timeTicket: null }
     }
     fetchNewDate () {
         let axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
         let option = this.state.option;
         let data0 = option.series[0].data;
         let data1 = option.series[1].data;
+        let data2 = option.series[2].data;
         data0.shift();
         data0.push(Math.round(Math.random() * 10));
         data1.shift();
         data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
-
+        data2.shift();
+        data2.push(Math.round(Math.random() * 10));
         option.xAxis[0].data.shift();
         option.xAxis[0].data.push(axisData);
-        option.xAxis[1].data.shift();
         this.setState({option: option});
     }
-    
-    componentDidMount() {
-        this.state.timeTicket ? clearInterval(this.state.timeTicket) : "";
-        this.state.timeTicket = setInterval(this.fetchNewDate.bind(this), 5000);
-    }
-    componentWillUnmount() {
-        this.state.timeTicket ? clearInterval(this.state.timeTicket) : "";
-    }
-    render() {
+    componentWillMount() {
         const { dynamicSeries, dynamicXAxis, dynamicYAxis } = this.props;
-        console.log(this.props);
         var series = [];
         const seriesBar = {
-            xAxisIndex: 1,
-            yAxisIndex: 1,
-            itemStyle: {
-                normal: {
-                    color: '#0ff',
-                    barBorderRadius: 4,
-                    opacity: '0.6'
-                },
-                emphasis: {
-                    opacity: '1'
-                }
+            itemStyle: { 
+                normal: { color: '#0ff', barBorderRadius: 4, opacity: '0.6' },
+                emphasis: { opacity: '1' }
             },
             animationEasing: 'elasticOut',
-            animationDelay: function (idx) {
-                return idx * 10;
-            },
-            animationDelayUpdate: function (idx) {
-                return idx * 10;
-            }
+            animationDelay: function (idx) { return idx * 10 },
+            animationDelayUpdate: function (idx) { return idx * 10 }
         };
         const seriesLine = {
-            name:'游客数量',
-            itemStyle: {
-                normal: {
-                    color: '#ffe729'
-                }
-            }
+            itemStyle: { normal: { color: '#ffe729' } }
         }
         const yAixsConf = dynamicYAxis.map((item) => {
             item.type = 'value';
@@ -88,32 +60,31 @@ class DynamicChartComponent extends Component {
             return item;
         });
         const legendData = dynamicSeries.map((item, i) => {
-            series[i] = item.type == 'bar' ? extend(seriesBar, item) : extend(seriesLine, item);
+            console.log(seriesBar);
+            console.log(item);
+            series[i] = extendObject(item.type == 'bar' ? seriesBar : seriesLine, item);
+            console.log(series[0].name)
             return item.name;
         });
-
+        console.log(series);
         const option = {
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                data:legendData,
-                textStyle: { color: '#fff' },
-            },
-            grid: {
-                top: 60,
-                left: 30,
-                right: 60,
-                bottom:30
-            },
+            tooltip: { trigger: 'axis' },
+            legend: { data: legendData, textStyle: { color: '#fff' } },
+            grid: { top: 60, left: 30, right: 60, bottom:30 },
             xAxis: xAixsConf,
             yAxis: yAixsConf,
             series: series
         };
-        this.setState({
-            option: option
-        })
-
+        this.setState({ option: option });
+    }
+    componentDidMount() {
+        this.state.timeTicket ? clearInterval(this.state.timeTicket) : '';
+        this.state.timeTicket = setInterval(this.fetchNewDate.bind(this), 5000);
+    }
+    componentWillUnmount() {
+        this.state.timeTicket ? clearInterval(this.state.timeTicket) : '';
+    }
+    render() {
         return (
             <div className='examples'>
                 <div className='parent' style={{position: 'relative'}}>
