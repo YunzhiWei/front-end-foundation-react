7649f0b23f72843e220551c563127924443ef2b4
bruce-zxy <bruce_zxy@163.com>
2017-08-10 21:19:51 +0800

    none

--- src/pages/BigData.js
+++ src/pages/BigData.js
@@ -20,53 +20,37 @@ require('./BigData/data/mapLib').blockAreaMap.map((item) => {
 })
 
 
-function TimerFunction() {
-    if (this.state.chartIndex === 0) this.setState({
-        chartIndex: 1
-    });
-    else if (this.state.chartIndex === 1) this.setState({
-        chartIndex: 2
-    });
-    else this.setState({
-        chartIndex: 0
-    });
-}
+// function TimerFunction() {
+//     if (this.state.chartIndex === 0) this.setState({
+//         chartIndex: 1
+//     });
+//     else if (this.state.chartIndex === 1) this.setState({
+//         chartIndex: 2
+//     });
+//     else this.setState({
+//         chartIndex: 0
+//     });
+// }
 
 class BigData extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            chartIndex: 0,
-            timeTicket: null
-        }
-    }
-    componentDidMount() {
-        this.state.timeTicket && clearInterval(this.state.timeTicket);
-        this.setState({
-            timeTicket: setInterval(TimerFunction.bind(this), 5000)
-        });
-    }
-    componentWillUnmount() {
-        this.state.timeTicket && clearInterval(this.state.timeTicket);
-    }
     render() {
         return (
             <MuiThemeProvider>
   				<div className="large-screen">
       				<CardProvider className="col-md-6 col-lg-4" title="折线图和柱状图" color="light-green">
-      					<ReactEcharts ref='echarts_react' option={echartsOption(BarLinesDataArray[this.state.chartIndex], 'BarLines')} />
+      					         <ReactEcharts ref='echarts_react' option={echartsOption(BarLinesDataArray[1], 'BarLines')} style={{height: '100%'}} />
       				</CardProvider>
       				<CardProvider className="col-md-6 col-lg-4" title="江西省年均降雨量" color="bright-white">
-                        <ReactEcharts ref='echarts_react' option={echartsOption(BlockAreaDataArray[1], 'SalesVolume')} />
+                        <ReactEcharts ref='echarts_react' option={echartsOption(BlockAreaDataArray[1], 'SalesVolume')} style={{height: '100%'}} />
       				</CardProvider>
       				<CardProvider className="col-md-6 col-lg-4" title="国内游客去向" color="bright-green">
-                        <ReactEcharts ref='echarts_react' option={echartsOption(FromToLinesDataArray[1], 'AirportCoordComponent')} />
+                        <ReactEcharts ref='echarts_react' option={echartsOption(FromToLinesDataArray[1], 'AirportCoordComponent')} style={{height: '100%'}} />
       				</CardProvider>
                     <CardProvider className="col-md-12 col-lg-8" title="游客与游船数量" color="light-pink">
-                        <ReactEcharts ref='echarts_react' option={echartsOption(dynamicChart, 'DynamicChart')} />
+                        <ReactEcharts ref='echarts_react' option={echartsOption(dynamicChart, 'DynamicChart')} style={{height: '100%'}} />
       				</CardProvider>
                     <CardProvider className="col-md-6  col-lg-4" title="预算与开销" color="purplish-red">
-                        <ReactEcharts ref='echarts_react' option={echartsOption(radarDataArray[0], 'RadarChart')} />
+                        <ReactEcharts ref='echarts_react' option={echartsOption(radarDataArray[0], 'RadarChart')} style={{height: '100%'}} />
       				</CardProvider>
   			    </div>
 			</MuiThemeProvider>
