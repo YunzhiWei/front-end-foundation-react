cfe3d98f9646351d6cfb809ddfe39cd8f1ac964b
bruce-zxy <bruce_zxy@163.com>
2017-10-16 17:20:10 +0800

    step over

--- src/pages/BigData.js
+++ src/pages/BigData.js
@@ -5,16 +5,12 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CardProvider from './mui/CardProvider';
 
 import LineAndHistogram from './BigData/LineAndHistogram';
-import DynamicChartComponent from './BigData/DynamicChartComponent';
 import RadarChart from './BigData/RadarChart';
 import WeiboData from './BigData/WeiboData';
 import ParkingLotComponent from './BigData/ParkingLot';
-import WeatherForeComponent from './BigData/WeatherFore';
 import NumOfPassComponent from './BigData/NumOfPass';
 import AirQualityComponent from './BigData/AirQuality';
 import ResUtilizationComponent from './BigData/ResUtilization';
-import TransportationComponent from './BigData/Transportation';
-import SankeyComponent from './BigData/Sankey';
 import WeatherComponent from './BigData/Weather';
 import TicketBusinessComponent from './BigData/TicketBusiness';
 import TimeViewComponent from './BigData/TimeView';
@@ -27,55 +23,55 @@ import weiboData from './BigData/data/weibo.json';
 
 class BigData extends Component {
     render() {
-       		return (
-        			<MuiThemeProvider>
-          				<div className="large-screen">
-                      <div className="bigdata_title">
-                          <span className="bigdata_l"></span>
-                          <span className="bigdata_c">仙女湖景区智慧旅游大数据</span>
-                          <span className="bigdata_r"></span>
-                      </div>
-                      <div className="bigdata_content">
-                          <div className="c_content c_1">
-                              <CardProvider className="cards" title="仙女湖景区票务统计" color="light-green">
-                                  <TicketBusinessComponent />
-                              </CardProvider>
-                              <CardProvider className="cards" title="仙女湖景区车位管理" color="light-green">
-                                  <ParkingLotComponent />
-                              </CardProvider>
-                              <CardProvider className="cards" title="仙女湖景区车位收费管理" color="light-green">
-                                  <ParkingChargeComponent />
-                              </CardProvider>
-                          </div>
-                          <div className="c_content c_2">
-                              <WeiboData weiboData={weiboData} />
-                              <CardProvider className="cards" title="仙女湖景区人员流量" color="light-green">
-                                  <NumOfPassComponent />
-                              </CardProvider>
-                          </div>
-                          <div className="c_content c_3">
-                              <TimeViewComponent />
-                              <ResUtilizationComponent />
-                              <CardProvider className="cards" title="仙女湖景区人员密度" color="light-green">
-                                  <RadarChart />
-                              </CardProvider>
-                          </div>
-                          <div className="c_content c_4">
-                              <CardProvider className="cards" title="仙女湖景区天气预报" color="light-green">
-                                  <WeatherComponent />
-                              </CardProvider>
-                              <CardProvider className="cards" title="仙女湖景区空气质量" color="light-green">
-                                  <AirQualityComponent />
-                              </CardProvider>
-                              <CardProvider className="cards" title="仙女湖景区客源地统计" color="light-green">
-                                  <LineAndHistogram BarLinesData={BarLinesDataArray[1]} />
-                              </CardProvider>
-                          </div>
-                      </div>
-          			  </div>
-        			</MuiThemeProvider>
-      		)
-    	}
+     		return (
+      			<MuiThemeProvider>
+        				<div className="large-screen">
+                    <div className="bigdata_title">
+                        <span className="bigdata_l"></span>
+                        <span className="bigdata_c">仙女湖景区智慧旅游大数据</span>
+                        <span className="bigdata_r"></span>
+                    </div>
+                    <div className="bigdata_content">
+                        <div className="c_content c_1">
+                            <CardProvider className="cards" title="仙女湖景区票务统计" color="light-green">
+                                <TicketBusinessComponent />
+                            </CardProvider>
+                            <CardProvider className="cards" title="仙女湖景区车位管理" color="light-green">
+                                <ParkingLotComponent />
+                            </CardProvider>
+                            <CardProvider className="cards" title="仙女湖景区车位收费管理" color="light-green">
+                                <ParkingChargeComponent />
+                            </CardProvider>
+                        </div>
+                        <div className="c_content c_2">
+                            <WeiboData weiboData={weiboData} />
+                            <CardProvider className="cards" title="仙女湖景区人员流量" color="light-green">
+                                <NumOfPassComponent />
+                            </CardProvider>
+                        </div>
+                        <div className="c_content c_3">
+                            <TimeViewComponent />
+                            <ResUtilizationComponent />
+                            <CardProvider className="cards" title="仙女湖景区人员密度" color="light-green">
+                                <RadarChart />
+                            </CardProvider>
+                        </div>
+                        <div className="c_content c_4">
+                            <CardProvider className="cards" title="仙女湖景区天气预报" color="light-green">
+                                <WeatherComponent />
+                            </CardProvider>
+                            <CardProvider className="cards" title="仙女湖景区空气质量" color="light-green">
+                                <AirQualityComponent />
+                            </CardProvider>
+                            <CardProvider className="cards" title="仙女湖景区客源地统计" color="light-green">
+                                <LineAndHistogram BarLinesData={BarLinesDataArray[1]} />
+                            </CardProvider>
+                        </div>
+                    </div>
+        			  </div>
+      			</MuiThemeProvider>
+    		)
+  	}
 }
 
 export default BigData;
